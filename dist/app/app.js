f184d93045cb889823ccb9072e4695904d2819e8
shishir-yh <shishiryh@gmail.com>
2025-06-17 01:59:30 +0600

    14-11 Error Handling in Express: 404s & Global Error Handler

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,19 +1,49 @@
 "use strict";
 /* Initial file of express js --> get , post , delete,search */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const todos_routes_1 = require("./todos/todos.routes");
 const app = (0, express_1.default)();
 app.use(express_1.default.json()); // perser , it it used to make the json file into object
 // router related middleware :-
+const todosRouter = express_1.default.Router();
 const userRouter = express_1.default.Router();
-app.use("/todos", todos_routes_1.todosRouter);
+app.use("/todos", todosRouter);
 app.use("/users", userRouter);
-app.get('/', (req, res) => {
-    res.send('I am learning express js and mongodb');
+app.get("/", (req, res) => {
+    res.send("I am learning");
+});
+/* lets work on error  */
+app.get("/error", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log(something);
+        res.send("welcome to error er duniya");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+/* When we can't find any route, we will give that error. */
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Sorry route is not found" });
+});
+/* how we handle the global error */
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log("error", error);
+        res.status(400).json({ message: "something is went wrong" });
+    }
 });
 exports.default = app;
 /*
@@ -21,4 +51,4 @@ server --> server handling like--> starting, closing, error handling of server. 
 app --> routing handling, middleware, route related error handling
 app folder --> business logic  handing like create read update delete, database related work
 
-*/ 
+*/
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,46 @@
 /* Initial file of express js --> get , post , delete,search */
 
-import express, { Application, Request, Response } from  "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import path from "path";
 import fs from "fs";
-import { todosRouter } from "./todos/todos.routes";
 
-const app:Application = express()
+const app: Application = express();
 
-app.use(express.json()) // perser , it it used to make the json file into object
+app.use(express.json()); // perser , it it used to make the json file into object
 
- // router related middleware :-
+// router related middleware :-
+const todosRouter = express.Router();
+const userRouter = express.Router();
 
-const userRouter = express.Router()
+app.use("/todos", todosRouter);
+app.use("/users", userRouter);
 
-app.use("/todos",todosRouter)
-app.use("/users",userRouter)
+app.get("/", (req: Request, res: Response) => {
+  res.send("I am learning");
+});
 
+/* lets work on error  */
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(something);
+    res.send("welcome to error er duniya");
+  } catch (error) {
+    next(error);
+  }
+});
 
+/* When we can't find any route, we will give that error. */
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Sorry route is not found" });
+});
 
-app.get('/', (req: Request, res : Response) => {
-    
-    res.send('I am learning express js and mongodb')
-})
-
-
-
+/* how we handle the global error */
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.log("error", error);
+    res.status(400).json({ message: "something is went wrong" });
+  }
+});
 
 export default app;
 
